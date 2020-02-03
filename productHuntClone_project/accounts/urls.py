d4508f859e0b85f7bfd579a444090cdca1916e42
Maxi Merrillees <maxi.merrillees@gmail.com>
2020-02-03 18:50:25 +1100

    Add sign up functionality

--- productHuntClone_project/accounts/urls.py
+++ productHuntClone_project/accounts/urls.py
@@ -5,6 +5,4 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('create_account/', views.create_account, name='create_account'),
-    path('login_account/', views.login_account, name='login_account'),
 ]
--- productHuntClone_project/accounts/views.py
+++ productHuntClone_project/accounts/views.py
@@ -1,17 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 def signup(request):
-    return render(request, 'accounts/signup.html')
+    # Create new account
+    if request.method == 'POST':
+        # Check that password match
+        if request.POST['password'] == request.POST['confirm_password']:
+            # Check for existing username
+            try:
+                # return render(request, 'products/home.html')
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'message': 'Username already exists'})
+            except User.DoesNotExist:
+                # Create new user account
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'])
+                auth.login(request, user)
+                return render(request, 'products/home.html', {'message': 'Account successfully created!'})
+        else:
+            return render(request, 'accounts/signup.html', {'message': 'Passwords do not match'})  
+    else:
+        # Go to sign up page
+        return render(request, 'accounts/signup.html')
 
 def login(request):
     return render(request, 'accounts/login.html')
-
-def create_account(request):
-    return
-
-def login_account(request):
-    return
 
 def logout(request):
     return render(request, 'products/home.html')
