5929e2e6e73636223484e3fd4c9b06c07bf3b124
Maxi Merrillees <maxi.merrillees@gmail.com>
2020-02-05 13:30:02 +1100

    Add login/logout funcitonality

--- productHuntClone_project/accounts/views.py
+++ productHuntClone_project/accounts/views.py
@@ -17,15 +17,28 @@ def signup(request):
                 # Create new user account
                 user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'])
                 auth.login(request, user)
-                return render(request, 'products/home.html', {'message': 'Account successfully created!'})
+                return redirect('home')
         else:
             return render(request, 'accounts/signup.html', {'message': 'Passwords do not match'})  
     else:
         # Go to sign up page
         return render(request, 'accounts/signup.html')
 
+
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        # Try to authenticate. If authenticated, returns valid User object
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html', {'message': 'Wrong username or password'})
+    else:
+        return render(request, 'accounts/login.html')
+
 
 def logout(request):
-    return render(request, 'products/home.html')
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
